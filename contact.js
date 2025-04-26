6d815fbf867a244c34ef31a0351ffb3b5486dfb9
Syeda Arsheen Fatima <arsheencoder78618888@gmail.com>
2025-04-26 10:06:03 +0530

    contact form page done

--- contact.js
+++ contact.js
@@ -21,6 +21,7 @@ router.post("/send-email", (req, res) => {
     products,
     Equipment,
     aftermarket,
+    note,
   } = req.body;
 
   console.log;
@@ -73,10 +74,70 @@ router.post("/send-email", (req, res) => {
       "arsheenstrong@gmail.com",
       "uzaid@obtechnos.com",
     ], // recipient
-    subject: "New Website Submission", // subject line
-    text: "You received a new message from the website! please ignore this message as this is only for testing", // plain text body
-    // html: "<p>You received a new message from the website!</p>" // Optional HTML
+    subject: "New Contact Form Submission", // subject line
+    html: `
+     <h2>New Contact Form Submission</h2>
+     <table border="1" cellpadding="10" cellspacing="0" style="border-collapse: collapse; width: 100%;">
+       <tr>
+         <th style="background-color: #337ab7; color: white;">Fields</th>
+         <th style="background-color: #337ab7; color: white;">Details</th>
+       </tr>
+       <tr><td><strong>Name</strong></td><td>${name}</td></tr>
+       <tr><td><strong>Email</strong></td><td>${email}</td></tr>
+       <tr><td><strong>Phone</strong></td><td>${phone}</td></tr>
+       <tr><td><strong>Company</strong></td><td>${company}</td></tr>
+       <tr><td><strong>Country</strong></td><td>${country}</td></tr>
+       <tr><td><strong>Product/Service</strong></td><td>${products}</td></tr>
+       <tr><td><strong>Equipment</strong></td><td>${
+         Equipment == "on" ? "Yes" : "No"
+       }</td></tr>
+       <tr><td><strong>Aftermarket & Modernization</strong></td><td>${
+         aftermarket == "on" ? "Yes" : "No"
+       }</td></tr>
+       <tr><td><strong>Message</strong></td><td>${note}</td></tr>
+     </table>
+   `,
   };
+
+  function capitalizeName(name) {
+    return name
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+
+  const capitalizedName = capitalizeName(name);
+
+  const mailOptionsToUser = {
+    from: '"Website Contact" <website@cemtecgroup.at>',
+    to: email, // the email submitted in the form
+    subject: "Thank You for Contacting Us",
+    html: `
+      <p>Dear ${capitalizedName},</p>
+      <p>Thank you for reaching out to Cemtec, We’ve received your inquiry and appreciate your interest.</p>
+      <p> Our team will review your message and get back to you within 24–48 hours. </p>
+      <p>For urgent matters, feel free to call us at +43 7223 83620 or email us at info@cemtecgroup.com</p>
+      <p> We look forward to assisting you!</p>
+      <p>Best regards,<br>
+       Team CEMTEC </p>
+
+       <hr style="margin-top:20px; margin-bottom:10px;">
+    <p style="font-size: 14px;">
+      <a href="https://www.cemtecgroup.at" target="_blank">www.cemtecgroup.at</a> | 
+      <a href="mailto:info@cemtecgroup.com">info@cemtecgroup.com</a> | 
+      <a href="tel:+43722383620">+43 7223 83620</a>
+    </p>
+    `,
+  };
+
+  transporter.sendMail(mailOptionsToUser, (error, info) => {
+    if (error) {
+      console.error("Error sending to user:", error);
+    } else {
+      console.log("User thank-you email sent:", info.response);
+    }
+  });
 
   // const mailOptions = {
   //   from: '"Website Contact" <website@cemtecgroup.at>',
